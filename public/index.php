1ec53fd904e6aae80d1c6d03a27fa6705a88dd94
Baldr <baldr.volsk@gmail.com>
2018-06-07 12:48:22 +0400

    добавил выбор окружения, поправил пути

--- public/index.php
+++ public/index.php
@@ -1,40 +1,4 @@
 <?php
-/**
- * CodeIgniter
- *
- * An open source application development framework for PHP
- *
- * This content is released under the MIT License (MIT)
- *
- * Copyright (c) 2014 - 2018, British Columbia Institute of Technology
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- *
- * @package	CodeIgniter
- * @author	EllisLab Dev Team
- * @copyright	Copyright (c) 2008 - 2014, EllisLab, Inc. (https://ellislab.com/)
- * @copyright	Copyright (c) 2014 - 2018, British Columbia Institute of Technology (http://bcit.ca/)
- * @license	http://opensource.org/licenses/MIT	MIT License
- * @link	https://codeigniter.com
- * @since	Version 1.0.0
- * @filesource
- */
 
 /*
  *---------------------------------------------------------------
@@ -53,7 +17,22 @@
  *
  * NOTE: If you change these, also change the error_reporting() code below
  */
-	define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'development');
+switch ($_SERVER['HTTP_HOST'])
+{
+    case "production.com":
+        define('ENVIRONMENT','production');
+        break;
+    case "179471.simplecloud.ru":
+        define('ENVIRONMENT','testing');
+        break;
+    case "social.loc":
+        define('ENVIRONMENT','dev-baldr');
+        break;    
+    default:
+        define('ENVIRONMENT','development');
+}
+
+define('THEME','default');
 
 /*
  *---------------------------------------------------------------
@@ -66,6 +45,7 @@
 switch (ENVIRONMENT)
 {
 	case 'development':
+	case 'dev-baldr':
 		error_reporting(-1);
 		ini_set('display_errors', 1);
 	break;
@@ -73,14 +53,7 @@ switch (ENVIRONMENT)
 	case 'testing':
 	case 'production':
 		ini_set('display_errors', 0);
-		if (version_compare(PHP_VERSION, '5.3', '>='))
-		{
-			error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED & ~E_STRICT & ~E_USER_NOTICE & ~E_USER_DEPRECATED);
-		}
-		else
-		{
-			error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT & ~E_USER_NOTICE);
-		}
+		error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED & ~E_STRICT & ~E_USER_NOTICE & ~E_USER_DEPRECATED);
 	break;
 
 	default:
@@ -97,7 +70,8 @@ switch (ENVIRONMENT)
  * This variable must contain the name of your "system" directory.
  * Set the path if it is not in the same directory as this file.
  */
-	$system_path = '../system';
+	define('SERVERROOT', dirname(__FILE__, 2));
+	$system_path = SERVERROOT.DIRECTORY_SEPARATOR.'system';
 
 /*
  *---------------------------------------------------------------
@@ -114,7 +88,7 @@ switch (ENVIRONMENT)
  *
  * NO TRAILING SLASH!
  */
-	$application_folder = '../application';
+	$application_folder = SERVERROOT.DIRECTORY_SEPARATOR.'application';
 
 /*
  *---------------------------------------------------------------
@@ -129,8 +103,7 @@ switch (ENVIRONMENT)
  *
  * NO TRAILING SLASH!
  */
-	$view_folder = '';
-
+	$view_folder = SERVERROOT.DIRECTORY_SEPARATOR.'views'.DIRECTORY_SEPARATOR.THEME;
 
 /*
  * --------------------------------------------------------------------
