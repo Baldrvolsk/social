f50f4dff5c2ea805e4c26be59cb1debf3419de8a
Baldr <baldr.volsk@gmail.com>
2018-06-10 23:39:24 +0400

    add WebHook

---
+++ application/config/testing/webhook.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Baldr
+ * Date: 10.06.2018
+ * Time: 22:03
+ */
+
+// Проверочный ключ
+$whKey = "V6A9BBND5PiSHf9ExuIUhtnC0n6oE8Bd";
+// ветка которая отслеживается
+$needBranch = 'test';
+// полный путь до лог-файла включая имя файла
+$log_file = APPPATH.DIRECTORY_SEPARATOR.'logs'.DIRECTORY_SEPARATOR.'deploy.log';
+
+$run_file = APPPATH.DIRECTORY_SEPARATOR.'logs'.DIRECTORY_SEPARATOR.'deploy_run.txt';
