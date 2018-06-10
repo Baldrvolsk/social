1e12b1815af8c523b338d3bcf2a93e80d125a218
Baldr <baldr.volsk@gmail.com>
2018-06-11 01:47:39 +0400

    edit config

--- application/config/webhook.php
+++ application/config/webhook.php
@@ -7,10 +7,10 @@
  */
 
 // Проверочный ключ
-$whKey = "V6A9BBND5PiSHf9ExuIUhtnC0n6oE8Bd";
+$config['whKey'] = "V6A9BBND5PiSHf9ExuIUhtnC0n6oE8Bd";
 // ветка которая отслеживается
-$needBranch = 'dev';
+$config['needBranch'] = 'dev';
 // полный путь до лог-файла включая имя файла
-$log_file = APPPATH.DIRECTORY_SEPARATOR.'logs'.DIRECTORY_SEPARATOR.'deploy.log';
+$config['log_file'] = APPPATH.DIRECTORY_SEPARATOR.'logs'.DIRECTORY_SEPARATOR.'deploy.log';
 
-$run_file = APPPATH.DIRECTORY_SEPARATOR.'logs'.DIRECTORY_SEPARATOR.'deploy_run.txt';
+$config['$run_file'] = APPPATH.DIRECTORY_SEPARATOR.'logs'.DIRECTORY_SEPARATOR.'deploy_run.txt';
