1bb92b8c447de67820f29fac0f14ffebe8cb662c
Baldr <baldr.volsk@gmail.com>
2018-06-11 00:10:30 +0400

    add Migration

--- application/controllers/Migrate.php
+++ application/controllers/Migrate.php
@@ -6,7 +6,15 @@
  * Time: 0:09
  */
 
-class Migrate
+class Migrate extends CI_Controller
 {
+
+    public function index() {
+        $this->load->library('migration');
+
+        if ($this->migration->current() === FALSE) {
+            show_error($this->migration->error_string());
+        }
+    }
 
 }
