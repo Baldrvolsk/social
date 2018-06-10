0f65f5c68eb82ce47365817f44fe8fa30cc92c4c
Baldr <baldr.volsk@gmail.com>
2018-06-11 00:28:19 +0400

    delete

--- application/config/testing/autoload.php
+++
@@ -1,3 +0,0 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-$autoload['libraries'] = array('database','ion_auth','session','migration');
--- application/config/testing/migration.php
+++
@@ -1,5 +0,0 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-$config['migration_enabled'] = true;
-$config['migration_type'] = 'sequential';
-$config['migration_auto_latest'] = true;
