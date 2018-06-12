169dbf9a147fa3d12f5fbbc2b047ffb35dbd6fe9
Baldr <baldr.volsk@gmail.com>
2018-06-12 11:52:51 +0400

    fix error

--- views/default/post/add_post.php
+++ views/default/post/add_post.php
@@ -1,10 +1,7 @@
 <form id="add_post" action="/post/add" method="POST" enctype="multipart/form-data">
     <input type="hidden" name="userId" value="<?=$userId?>">
     <div class="form-group">
-        <label for="PostContent"></label>
-        <textarea name="content" class="form-control" id="PostContent" placeholder="Что у вас нового" required>
-            <?=trim(set_value('content'))?>
-        </textarea>
+        <textarea name="content" class="form-control" id="PostContent" placeholder="Что у вас нового" required><?=set_value('content')?></textarea>
         <?php if(form_error('content')) {echo form_error('content','<span class="error">','</span>');} ?>
     </div>
     <button type="submit" class="btn btn-primary">Поделиться</button>
