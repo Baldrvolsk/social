ff4925608caefacffdbbdeba01d10167ef46bc4e
Baldr <baldr.volsk@gmail.com>
2018-06-12 15:41:54 +0400

    add friend controller && model

--- application/controllers/Friends.php
+++ application/controllers/Friends.php
@@ -6,7 +6,31 @@
  * Time: 12:49
  */
 
-class Friends
+class Friends extends CI_Controller
 {
+    public $user;
+    public function __construct() {
+        parent::__construct();
+        $this->load->model('friend_model');
+        if (!$this->ion_auth->logged_in()) {
+            // redirect them to the login page
+            redirect('auth/login', 'refresh');
+        }
+        $this->user = $this->ion_auth->user()->row();
+        $photos = scandir('./uploads/profile/'.$this->user->id);
 
+        $this->user->photo = end($photos);
+    }
+
+    public function index() {
+        $data['friends'] = $this->friend_model->get_friends($this->user->id);
+        $data['users'] = $this->ion_auth->users()->result();
+        $this->load->view('header', $data);
+        $this->load->view('friend/list');
+        $this->load->view('footer');
+    }
+
+    public function add_friend($id, $group_id) {
+
+    }
 }
--- application/models/Friend_model.php
+++ application/models/Friend_model.php
@@ -6,55 +6,33 @@
  * Time: 14:03
  */
 
-class Post_model extends CI_Model
+class Friend_model extends CI_Model
 {
-    private $post_table = 'post';
-    private $comm_table = 'post_comment';
+    private $friend_table = 'friends';
+    private $fr_group_table = 'friend_group';
     public function __construct() {
 
     }
 
-    public function get_post($id = null) {
-        if ($id === null) {
-            $query = $this->db->get($this->post_table);
-            return $query->result_array();
-        }
-
-        $query = $this->db->get_where($this->post_table, array('id' => $id));
-        return $query->row_array();
-    }
-    public function get_users_post($user_id = null, $limit = null, $offset = null) {
+    public function get_friends($user_id = null, $fr_group = null) {
         $user_id = isset($user_id) ? $user_id : $this->session->userdata('user_id');
-        if ($limit !== null && $limit !== 0) {
-            if ($offset === null) $offset = 0;
-            $this->db->join('users', 'users.id = post.user_id');
-            $this->db->order_by($this->post_table.'.date_add', 'desc');
-            $this->db->where($this->post_table.'.user_id', $user_id);
-            $query = $this->db->get($this->post_table, $limit, $offset);
-            return $query->result_array();
+        if ($fr_group !== null) {
+            $this->db->where($this->friend_table . '.friend_group_id', $user_id);
+            $this->db->join($this->fr_group_table, $this->fr_group_table.'.id = '.$this->friend_table.'.friend_group_id');
         }
+        $this->db->join('users', 'users.id = '.$this->friend_table.'.friend_id');
+        $this->db->where($this->friend_table.'.user_id', $user_id);
+        $this->db->order_by($this->friend_table.'.date_add', 'desc');
+        $query = $this->db->get($this->friend_table);
+        return $query->result_array();
     }
 
-    public function createPost() {
-        $content = trim($this->input->post('content', true));
-        //$this->load->helper('url');
-
-        //$slug = url_title($this->input->post('title'), 'dash', TRUE);
-        $link = null;
-        $tags = null;
+    public function add_friend($user_id, $friend_id, $friend_group_id = null) {
         $data = array(
-            'user_id' => $this->input->post('userId'),
+            'user_id' => $user_id,
+            'friend_id' => $friend_id,
             'date_add' => mdate('%Y-%m-%d %H:%i:%s', now()),
-            'date_edit' => mdate('%Y-%m-%d %H:%i:%s', now()),
-            'content' => $content,
-            'link' => $link,
-            'tags' => $tags,
-            'views' => 0,
-            'like' => 0,
-            'dislike' => 0,
-            'comments' => 0,
-            'repost' => 0,
-            'no_comm' => 0,
+            'friend_group_id' => $friend_group_id,
         );
 
         return $this->db->insert('post', $data);
