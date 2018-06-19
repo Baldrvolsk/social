b236629f70c2eb431125611788bc218b9aa36f34
Ivan <l0mer0k@bk.ru>
2018-06-19 15:19:13 +0300

    add field

--- application/migrations/008_l_create_photos.php
+++ application/migrations/008_l_create_photos.php
@@ -3,8 +3,9 @@
 class Migration_create_photos extends CI_Migration {
 
     public function up() {
-        $this->db->query('CREATE TABLE `albums` ( `id` INT NOT NULL AUTO_INCREMENT , `user_id` INT NOT NULL , `name` VARCHAR(255) NOT NULL , `date` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP , `status` INT NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;');
-        $this->db->query('CREATE TABLE `photos` ( `id` INT NOT NULL AUTO_INCREMENT , `user_id` INT NOT NULL , `album_id` INT NOT NULL , `date` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP , `description` LONGTEXT NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;');
+        $this->db->query('CREATE TABLE `albums` ( `id` INT NOT NULL AUTO_INCREMENT , `user_id` INT NOT NULL , `name` VARCHAR(255) NOT NULL, `description` LONGTEXT , `date` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP , `status` INT NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;');
+
+        $this->db->query('CREATE TABLE `photos` ( `id` INT NOT NULL AUTO_INCREMENT , `user_id` INT NOT NULL , `album_id` INT NOT NULL , `date` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP , `description` LONGTEXT NOT NULL, `file` VARCHAR(255) NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;');
     }
 
     public function down() {
