cbfef1630df7c06adee161e648a7267924f5ee4c
Baldr <baldr.volsk@gmail.com>
2018-06-29 06:16:25 +0400

    edit people

--- application/controllers/People.php
+++ application/controllers/People.php
@@ -13,18 +13,37 @@ class People extends CI_Controller
     }
 
     public function index() {
-        $data['people'] = $this->ion_auth
-            ->select('id, concat(users.first_name," ",users.last_name) as full_name_user, users.company as photo')
-            ->limit(10)
-            ->users()->result();
-        $data['on_people'] = $this->ion_auth
-            ->select('id, concat(users.first_name," ",users.last_name) as full_name_user, users.company as photo')
-            ->limit(10)
-            ->users()->result();
-        //echo '<pre>'.var_dump($data).'</pre>';die();
+        $data['people'] = $this->get_people();
+        $data['on_people'] = $this->get_people(10, 0, true);
+
         $this->load->view('header', $data);
         $this->load->view('people/index');
         $this->load->view('footer');
+    }
+
+    public function get_people($limit = 10, $offset = 0, $online = false) {
+        $where["users.id !="] = $this->user->id;
+        if ($online) $where['users_meta.online'] = true;
+        return $this->db
+            ->select('users.id as id,
+                 concat(users.first_name," ",users.last_name) as full_name_user,
+                 users.company as photo,
+                 f_u.friend_status as f_u_status,
+                 u_f.friend_status as u_f_status,
+                 users_meta.online as online')
+            ->from('users')
+            ->join('friends as f_u',
+                   '(f_u.user_id = users.id AND f_u.friend_id = '.$this->user->id.')',
+                    'left')
+            ->join('friends as u_f',
+                   '(u_f.user_id = '.$this->user->id.' AND u_f.friend_id = users.id)',
+                   'left')
+            ->join('users_meta',
+                   'users_meta.id = users.id',
+                   'left')
+            ->where($where)
+            ->limit($limit, $offset)
+            ->get()->result();
     }
 
 }
