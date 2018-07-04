3db648c3bdff56ebc2016af35c4a303bda0f9c11
Baldr <baldr.volsk@gmail.com>
2018-07-04 16:02:46 +0400

    добавил контроллер баланса

--- application/controllers/Balance.php
+++ application/controllers/Balance.php
@@ -1,7 +1,24 @@
 <?php
 
 
-class Balance
+class Balance extends CI_Controller
 {
+    public $user;
 
+    public function __construct() {
+        parent::__construct();
+        $this->load->library('form_validation');
+        $this->load->model('group_model');
+        if (!$this->ion_auth->logged_in()) {
+            // redirect them to the login page
+            redirect('auth/login', 'refresh');
+        }
+        $this->user = $this->ion_auth->user()->row();
+    }
+
+    public function index() {
+        $this->load->view('header');
+        $this->load->view('balance/index');
+        $this->load->view('footer');
+    }
 }
