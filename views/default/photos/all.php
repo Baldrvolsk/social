2ad954e8c68f669fdb21d6de21413186abb75486
Ivan <l0mer0k@bk.ru>
2018-07-05 14:13:02 +0300

    Gallery for photos

--- views/default/photos/all.php
+++ views/default/photos/all.php
@@ -12,7 +12,9 @@
         <h3>Photos</h3>
         <div class="row">
         <?php foreach($photos as $p) : ?>
-            <div class="col-md-3"><img src="<?=$p->file;?>" style="width:100%"/><span><?=$p->description;?></span></div>
+            <div class="col-md-3">
+                <a href="<?=$p->file;?>" class="gal" rel="gal"><img src="<?=$p->file;?>" style="width:100%"/></a>
+                <span><?=$p->description;?></span></div>
         <?php endforeach; ?>
         </div>
     </div>
@@ -84,3 +86,6 @@
         </div>
     </div>
 </div>
+    <link href="/css/colorbox.css" rel="stylesheet">
+    <script type="text/javascript" src="/js/jquery.colorbox-min.js"></script>
+    <script type="text/javascript" src="/js/photos.js"></script>
