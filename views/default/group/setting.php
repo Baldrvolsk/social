39536a812c800b6d8da77bf7b288402d8ffcb06e
Baldr <baldr.volsk@gmail.com>
2018-07-18 17:57:04 +0400

    шаблон модалки для настроек группы

--- views/default/group/setting.php
+++ views/default/group/setting.php
@@ -1,2 +1,11 @@
-<?php
-
+<div class="modal-header">
+    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+    <h4 class="modal-title">Название модали</h4>
+</div>
+<div class="modal-body">
+    <p>One fine body&hellip;</p>
+</div>
+<div class="modal-footer">
+    <button type="button" class="btn btn-default" data-dismiss="modal">Закрыть</button>
+    <button type="button" class="btn btn-primary">Сохранить изменения</button>
+</div>
