9d8387cc23d3a36adf5abbbeb172c6ab5317ee5c
Baldr <baldr.volsk@gmail.com>
2018-07-18 20:05:48 +0400

    fix group post migrate

--- application/migrations/0017_b_add_group_post.php
+++ application/migrations/0017_b_add_group_post.php
@@ -8,10 +8,8 @@ class Migration_add_group_post extends CI_Migration {
         $this->dbforge->add_field(array(
             'group_id' => array('type' => 'INT'),
             'user_create_id' => array('type' => 'INT'),
-            //'date_add' => array('type' => 'TIMESTAMP'),
-            'date_add TIMESTAMP DEFAULT CURRENT_TIMESTAMP',
-            //'date_edit' => array('type' => 'TIMESTAMP'),
-            'date_edit TIMESTAMP ON UPDATE CURRENT_TIMESTAMP',
+            '`date_add` TIMESTAMP DEFAULT CURRENT_TIMESTAMP',
+            '`date_edit` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP',
             'content' => array('type' => 'TEXT', 'null' => TRUE),
             'link' => array('type' => 'TEXT', 'null' => TRUE),
             'tags' => array('type' => 'TEXT', 'null' => TRUE),
@@ -23,38 +21,20 @@ class Migration_add_group_post extends CI_Migration {
             'no_comm' => array('type' => 'BOOLEAN', 'default' => false),
         ));
         $this->dbforge->create_table('community_post');
-        /*
-        $sql = 'ALTER TABLE `community_post` 
-                CHANGE `date_add` `date_add` TIMESTAMP 
-                NOT NULL DEFAULT CURRENT_TIMESTAMP';
-        $this->db->query($sql);
-        $sql1 = 'ALTER TABLE `community_post` 
-                CHANGE `date_edit` `date_edit` TIMESTAMP on update CURRENT_TIMESTAMP 
-                NOT NULL DEFAULT CURRENT_TIMESTAMP';
-        $this->db->query($sql1);
-        */
+
         // Table structure for table 'community_post_comment'
         $this->dbforge->add_field('id');
         $this->dbforge->add_field(array(
             'post_id' => array('type' => 'INT'),
             'user_id' => array('type' => 'INT'),
-            'date_add' => array('type' => 'TIMESTAMP'),
-            'date_edit' => array('type' => 'TIMESTAMP'),
+            '`date_add` TIMESTAMP DEFAULT CURRENT_TIMESTAMP',
+            '`date_edit` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP',
             'content' => array('type' => 'TEXT', 'null' => TRUE),
             'tags' => array('type' => 'TEXT', 'null' => TRUE),
             'like' => array('type' => 'INT'),
             'dislike' => array('type' => 'INT'),
         ));
         $this->dbforge->create_table('community_post_comment');
-
-        $sql2 = 'ALTER TABLE `community_post_comment` 
-                CHANGE `date_add` `date_add` TIMESTAMP 
-                NOT NULL DEFAULT CURRENT_TIMESTAMP';
-        $this->db->query($sql2);
-        $sql3 = 'ALTER TABLE `community_post_comment` 
-                CHANGE `date_edit` `date_edit` TIMESTAMP on update CURRENT_TIMESTAMP 
-                NOT NULL DEFAULT CURRENT_TIMESTAMP';
-        $this->db->query($sql3);
 
         // Table structure for table 'community_post_like'
         $this->dbforge->add_field('id');
