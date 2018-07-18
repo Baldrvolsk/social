96c6d516e6f2c2451937390b95bac8cf5a244b0f
Baldr <baldr.volsk@gmail.com>
2018-07-18 18:08:07 +0400

    контроллер для работы с балансом, реализовано тестовое пополнение

--- application/controllers/Balance.php
+++ application/controllers/Balance.php
@@ -8,7 +8,7 @@ class Balance extends CI_Controller
     public function __construct() {
         parent::__construct();
         $this->load->library('form_validation');
-        $this->load->model('group_model');
+        $this->load->model('balance_model');
         if (!$this->ion_auth->logged_in()) {
             // redirect them to the login page
             redirect('auth/login', 'refresh');
@@ -20,5 +20,30 @@ class Balance extends CI_Controller
         $this->load->view('header');
         $this->load->view('balance/index');
         $this->load->view('footer');
+    }
+
+    public function add($user_id) {
+        $this->load->helper('form');
+        $this->load->library('form_validation');
+
+        $this->form_validation->set_rules('leptas', 'leptas', 'required|min_length[1]|max_length[5]');
+
+        if ($this->form_validation->run() === FALSE) {
+            $this->load->view('header');
+            $this->load->view('balance/index');
+            $this->load->view('footer');
+
+        } else {
+            $comment = 'Начисление с тестовой странички';
+            $result = $this->balance_model->add_balance($user_id, $this->input->post('leptas', true), $comment);
+            if ($result) {
+                $url = 'profile';
+                redirect($url);
+            } else {
+                $this->load->view('header');
+                $this->load->view('balance/index');
+                $this->load->view('footer');
+            }
+        }
     }
 }
