6b9ed7628ca20928b29a924a96bd4e6239b0805d
Baldr <baldr.volsk@gmail.com>
2018-07-18 18:04:11 +0400

    конфиг файл для групп

--- application/config/group.php
+++ application/config/group.php
@@ -1,2 +1,13 @@
 <?php
+//$config['group_'] = ;
 
+// стоимость создания группы
+$config['group_cost'] = 10;
+//время блокировки создания групп в формате DateInterval
+$config['group_create_interval'] = 'P1M';
+//правила для групп
+$config['group_rules'] = array(
+    'edit_setting',
+    'edit_rules',
+    ''
+);
