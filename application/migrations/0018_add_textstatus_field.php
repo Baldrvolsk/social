322eef5370ef84a4bfdc10dafc841feec4415041
Ivan <l0mer0k@bk.ru>
2018-07-19 16:31:55 +0300

    дата рождения в миграцию заодним

--- application/migrations/0018_add_textstatus_field.php
+++ application/migrations/0018_add_textstatus_field.php
@@ -4,6 +4,8 @@ class Migration_add_textstatus_field extends CI_Migration {
 
     public function up() {
         $this->db->query('ALTER TABLE `users`  ADD `text_status` VARCHAR(255) NOT NULL  AFTER `gender`');
+        $this->db->query('ALTER TABLE `users` ADD `birth` DATE NULL DEFAULT NULL AFTER `gender`');
+
     }
 
     public function down() {
