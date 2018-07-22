311091e468703164c9c0b822e715f5b3a0fcd873
Baldr <baldr.volsk@gmail.com>
2018-07-22 18:53:59 +0400

    edit

--- views/default/group/setting.php
+++ views/default/group/setting.php
@@ -1,11 +1,28 @@
 <div class="modal-header">
     <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-    <h4 class="modal-title">Название модали</h4>
+    <h4 class="modal-title">Настройки группы "<?=$group->name?>"</h4>
 </div>
-<div class="modal-body">
-    <p>One fine body&hellip;</p>
-</div>
-<div class="modal-footer">
-    <button type="button" class="btn btn-default" data-dismiss="modal">Закрыть</button>
-    <button type="button" class="btn btn-primary">Сохранить изменения</button>
+<div class="modal-body row">
+    <ul class="nav nav-tabs">
+        <li class="active"><a href="#general" data-toggle="tab">Основные</a></li>
+        <li><a href="#rules" data-toggle="tab">Привелегии</a></li>
+        <li><a href="#admin" data-toggle="tab">Администрация</a></li>
+    </ul>
+    <div class="tab-content">
+        <div class="tab-pane active" id="general">
+            <div class="row">
+
+            </div>
+        </div>
+        <div class="tab-pane" id="rules">
+            <div class="row">
+
+            </div>
+        </div>
+        <div class="tab-pane" id="admin">
+            <div class="row">
+
+            </div>
+        </div>
+    </div>
 </div>
