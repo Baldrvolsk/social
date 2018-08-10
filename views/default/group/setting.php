6f7aedd2e14c64d5c7474ea37066d6f28d98ef20
Baldr <baldr.volsk@gmail.com>
2018-08-10 14:05:25 +0400

    edit group manage page

--- views/default/group/setting.php
+++ views/default/group/setting.php
@@ -1,13 +1,17 @@
 <div class="row">
-    <p class="col-md-11 h3">Управление группой "<?=$group->name?>"</p>
-    <a class="col-md-1" href="/group/<?=$group->id?>" title="Назад на страницу группы">
-        <span class="glyphicon glyphicon-arrow-left"></span>
-    </a>
+    <p class="col-md-12 h3">
+        <a href="/group/<?=$group->id?>" title="Назад на страницу группы">
+            <span class="glyphicon glyphicon-arrow-left"></span>
+        </a>
+        &emsp;
+        Управление группой "<?=$group->name?>"</p>
+
 </div>
 <ul class="nav nav-tabs">
     <li class="active"><a href="#general" data-toggle="tab">Основные настройки</a></li>
     <li><a href="#rules" data-toggle="tab">Привелегии</a></li>
-    <li><a href="#admin" data-toggle="tab">Администрация</a></li>
+    <li><a href="#users" data-toggle="tab">Участники</a></li>
+    <li><a href="#remove" data-toggle="tab">Удалить группу</a></li>
 </ul>
 <div class="tab-content">
     <div class="tab-pane fade in active" id="general">
@@ -169,10 +173,101 @@
             </button>
         </form>
     </div>
-    <div class="tab-pane fade" id="admin">
-        <div class="row">
 
+    <div class="tab-pane fade" id="users">
+        <div class="row">
+            <div class="col-md-9">
+                <?php foreach ($users as $row) : ?>
+                <div class="row userCard<?=($row->community_group_id < 10)?' hidden':''?>"
+                     data-type-user="<?=$row->community_group_id?>">
+                    <div class="col-md-2">
+                        <img src="<?=$row->photo?>" width="50" class="center-block img-circle">
+                    </div>
+                    <div class="col-md-10">
+                        <p>
+                            <a href="/profile/<?=$row->id?>" title="Посмотреть профиль">
+                                <?= $row->full_name_user ?>
+                            </a>
+                        </p>
+                        <p>Ранг</p>
+                        <p>
+                            <a href="#" class="btn btn-info"
+                               title="test">
+                                Действие
+                            </a>
+                        </p>
+                    </div>
+                </div>
+                <?php endforeach; ?>
+            </div>
+            <div class="col-md-3">
+                <div class="radio">
+                    <input type="radio" class="radio-hidden" id="changeUserAll" name="changeUser" value="all"/>
+                    <label for="changeUserAll">Все участники</label>
+                </div>
+                <div class="radio">
+                    <input type="radio" class="radio-hidden" id="changeUserAdmin" name="changeUser" value="admin"/>
+                    <label for="changeUserAdmin">Администрация</label>
+                </div>
+                <div class="radio">
+                    <input type="radio" class="radio-hidden" id="changeUserRequest" name="changeUser" value="request"/>
+                    <label for="changeUserRequest">Заявки</label>
+                </div>
+                <div class="radio">
+                    <input type="radio" class="radio-hidden" id="changeUserBan" name="changeUser" value="ban"/>
+                    <label for="changeUserBan">Заблокированные</label>
+                </div>
+            </div>
         </div>
+    </div>
+
+    <div class="tab-pane fade" id="remove">
+        <div class="row">
+            <p class="col-md-9">
+                Вы можете удалить группу нажав на кнопку справа, либо передать
+                права участнику группы являющемуся администратором
+            </p>
+            <div class="col-md-3">
+                <button type="button" class="btn btn-block btn-success btn-md"
+                        onclick="removeGroup('<?=$group->id?>');" >
+                    Удалить группу
+                </button>
+            </div>
+        </div>
+
+        <?php
+        $admins = array();
+        foreach ($users as $row) {
+            if ($row->community_group_id == 50) {
+                $admins[] = $row;
+            }
+        }
+        if (count($admins) === 0) : ?>
+        <p>Для передачи прав в группе должен быть хотя бы один администратор</p>
+        <?php else : ?>
+        <form class="form-horizontal" id="group-transfer" enctype="multipart/form-data">
+            <div class="form-group">
+                <div class="col-sm-9">
+                    <select id="transfer" name="transfer" class="form-control">
+                        <option value="null">
+                            Выберите администратора которому передаете группу
+                        </option>
+                        <?php foreach ($admins as $row) : ?>
+                        <option value="<?=$row->user_id?>" ><?=$row->full_name_user?></option>
+                        <?php endforeach; ?>
+                    </select>
+                    <span class="text-danger" id="type_err" ></span>
+                </div>
+                <div class="col-md-3">
+                    <button type="button" class="btn btn-block btn-success btn-md"
+                            onclick="transferGroup('<?=$group->id?>');" disabled="disabled">
+                        Передать группу
+                    </button>
+                </div>
+            </div>
+            <div id="status" class="text-center"></div>
+        </form>
+        <?php endif; ?>
     </div>
 </div>
 <?php if (DEBUG) : ?>
@@ -181,7 +276,7 @@
         <div class="row">
             <?php
             ob_start();
-            var_dump($group);
+            var_dump($users);
             $s= ob_get_contents();
             ob_end_clean();
             echo '<pre>'.$s.'</pre>';
@@ -190,6 +285,11 @@
     </div>
 </div>
 <?php endif; ?>
+<style>
+    .radio-hidden {
+        display: none;
+    }
+</style>
 <script>
     function saveGroupInfo(id) {
         var formData = new FormData();
@@ -338,6 +438,134 @@
                     .removeClass("bg-danger bg-info bg-success")
                     .html('<span class="text-danger">Что-то пошло не так. Попробуйте позже.</span>')
                     .css({'margin-bottom':'10px','padding':'10px 0'});
+            }
+        });
+    }
+
+    let currSelect = null;
+    $(function() {
+        let defaultRadio = $('#users input:radio[value="all"]');
+        currSelect = initSelectRadio(defaultRadio);
+        $('#users').on('click', 'input:radio[name="changeUser"]', function() {
+            if ($(this).val() === currSelect.val()) {
+                return false;
+            } else {
+
+                currSelect = initSelectRadio($(this));
+                filterUser($(this).val());
+            }
+        });
+        $('#transfer option[value="null"]').prop({checked:true, disabled:true});
+        $('#group-transfer').on('change', '#transfer', function(){checkTransfer()});
+    });
+
+    function initSelectRadio(elem) {
+        elem.prop('checked', true);
+        elem.siblings().css("font-weight", "bolder");
+        $("#users input:radio:not(:checked)").siblings().css("font-weight", "normal");
+        return elem;
+    }
+
+    function filterUser(type) {
+        let users = document.getElementById('users');
+        let usersCard = users.getElementsByClassName('userCard');
+        for(let i=0; i<usersCard.length; i++) {
+            switch (type) {
+                case 'all':
+                    if (usersCard[i].dataset.typeUser >= 10) {
+                        usersCard[i].classList.remove("hidden")
+                    } else {
+                        usersCard[i].classList.add("hidden")
+                    }
+                    break;
+                case 'admin':
+                    if (usersCard[i].dataset.typeUser >= 20) {
+                        usersCard[i].classList.remove("hidden")
+                    } else {
+                        usersCard[i].classList.add("hidden")
+                    }
+                    break;
+                case 'request':
+                    if (usersCard[i].dataset.typeUser == 5) {
+                        usersCard[i].classList.remove("hidden")
+                    } else {
+                        usersCard[i].classList.add("hidden")
+                    }
+                    break;
+                case 'ban':
+                    if (usersCard[i].dataset.typeUser == 1) {
+                        usersCard[i].classList.remove("hidden")
+                    } else {
+                        usersCard[i].classList.add("hidden")
+                    }
+                    break;
+            }
+        }
+    }
+
+    function removeGroup(id) {
+        $.ajax({
+            url: "/group/remove_group/"+id,
+            type: "POST",
+            dataType: "json",
+            success: function (json) {
+                if (json.status == "OK") { //ошибок не было
+                    $('#Modal .modal-content').html(json.message);
+                    $('#Modal').modal('show');
+                    setTimeout(function() {
+                        $('#Modal').modal('hide');
+                        $('#Modal .modal-content').html('');
+                    }, 3000);
+                } else { //ошибки были, показываем их описание
+                    $('#Modal .modal-content').html(json.message);
+                    $('#Modal').modal('show');
+                }
+            }
+        });
+    }
+
+    function checkTransfer() {
+        let trId = $("#transfer option:selected").val();
+        $.ajax({
+            url: "/group/check_transfer/"+trId,
+            type: "POST",
+            dataType: "json",
+            success: function (json) {
+                if (json.status == "OK") { //ошибок не было
+                    $('#group-transfer button').removeProp('disabled');
+                } else { //ошибки были, показываем их описание
+                    $('#group-transfer #status').addClass("bg-danger")
+                        .removeClass("bg-success bg-info bg-warning")
+                        .html(json.message)
+                        .css({'margin-bottom':'10px','padding':'10px 0'});
+                }
+            }
+        });
+    }
+
+    function transferGroup(id) {
+        let formData = new FormData();
+        formData.append('transfer', $("#transfer option:selected").val());
+        $.ajax({
+            url: "/group/group_transfer/"+id,
+            type: "POST",
+            data: formData,
+            dataType: "json",
+            cache: false,
+            contentType: false,
+            processData: false,
+            success: function (json) {
+                if (json.status == "OK") { //ошибок не было
+                    $('#Modal .modal-content').html(json.message);
+                    $('#Modal').modal('show');
+                    setTimeout(function() {
+                        $('#Modal').modal('hide');
+                        $('#Modal .modal-content').html('');
+                    }, 3000);
+                } else { //ошибки были, показываем их описание
+                    $('#Modal .modal-content').html(json.message);
+                    $('#Modal').modal('show');
+                }
             }
         });
     }
