29449a06f746639eab966b260cb5d5299bbf1abf
Baldr <baldr.volsk@gmail.com>
2018-08-17 01:08:47 +0400

    edit path

--- views/default/admin/header.php
+++ views/default/admin/header.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="utf-8" />
-    <link rel="icon" type="image/png" href="/admin_assets/img/favicon.ico">
+    <link rel="icon" type="image/png" href="/assets/admin/img/favicon.ico">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 
     <title><?= $title; ?></title>
@@ -12,23 +12,23 @@
 
 
     <!-- Bootstrap core CSS     -->
-    <link href="/admin_assets/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="/assets/admin/css/bootstrap.min.css" rel="stylesheet" />
 
     <!-- Animation library for notifications   -->
-    <link href="/admin_assets/css/animate.min.css" rel="stylesheet"/>
+    <link href="/assets/admin/css/animate.min.css" rel="stylesheet"/>
 
     <!--  Light Bootstrap Table core CSS    -->
-    <link href="/admin_assets/css/light-bootstrap-dashboard.css?v=1.4.0" rel="stylesheet"/>
+    <link href="/assets/admin/css/light-bootstrap-dashboard.css?v=1.4.0" rel="stylesheet"/>
 
 
     <!--  CSS for Demo Purpose, don't include it in your project     -->
-    <link href="/admin_assets/css/demo.css" rel="stylesheet" />
+    <link href="/assets/admin/css/demo.css" rel="stylesheet" />
 
 
     <!--     Fonts and icons     -->
     <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
     <link href='http://fonts.googleapis.com/css?family=Roboto:400,700,300' rel='stylesheet' type='text/css'>
-    <link href="/admin_assets/css/pe-icon-7-stroke.css" rel="stylesheet" />
+    <link href="/assets/admin/css/pe-icon-7-stroke.css" rel="stylesheet" />
 
 </head>
 <body>
