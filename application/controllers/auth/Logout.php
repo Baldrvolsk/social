09130e315f4b2513185090f453181216f9837b5f
Baldr <baldr.volsk@gmail.com>
2018-08-17 00:59:51 +0400

    reformat code

--- application/controllers/auth/Logout.php
+++ application/controllers/auth/Logout.php
@@ -4,19 +4,16 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Logout extends CI_Controller
 {
 
-    public function __construct()
-    {
+    public function __construct() {
         parent::__construct();
         $this->load->library('form_validation');
-        if (!$this->ion_auth->logged_in())
-        {
+        if (!$this->ion_auth->logged_in()) {
             // redirect them to the login page
             redirect('auth/login');
         }
     }
 
-    public function index()
-    {
+    public function index() {
 
         $logout = $this->ion_auth->logout();
 
