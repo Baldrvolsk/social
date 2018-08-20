a58647e1b6b36e17d618441efe66635eb078ec5a
Baldr <baldr.volsk@gmail.com>
2018-08-21 02:06:22 +0400

    1. добавленны коментарии группам 2. добавлены поля профиля пользователя

--- application/language/russian/site_lang.php
+++ application/language/russian/site_lang.php
@@ -1,6 +1,8 @@
 <?php
+// SigIn button
 $lang['site_signIn'] = 'Войти в систему с Google';
 
+// Main menu
 $lang['site_menu_signIn'] = 'Войти';
 $lang['site_menu_rules'] = 'Правила';
 $lang['site_menu_forInvestors'] = 'Для инвесторов';
@@ -9,8 +11,27 @@ $lang['site_menu_home'] = 'На главную';
 $lang['site_menu_people'] = 'Люди';
 $lang['site_menu_groups'] = 'Группы';
 $lang['site_menu_search'] = 'Поиск';
+
+// Social panel
 $lang['site_social_text'] = 'Ищите нас';
 $lang['site_social_title_fb'] = 'Facebook';
 $lang['site_social_title_tw'] = 'Twitter';
 $lang['site_social_title_vk'] = 'Вконтакте';
 $lang['site_social_title_tel'] = 'Telegram';
+
+// User data
+$lang['site_user_data_email_label'] = 'Email';
+$lang['site_user_data_email_ph'] = 'Ваш email';
+$lang['site_user_data_login_label'] = 'Логин';
+$lang['site_user_data_login_ph'] = 'Ваш логин';
+$lang['site_user_data_fname_label'] = 'Имя';
+$lang['site_user_data_fname_ph'] = 'Ваше имя';
+$lang['site_user_data_lname_label'] = 'Фамилия';
+$lang['site_user_data_lname_ph'] = 'Ваша фамилия';
+$lang['site_user_data_avatar'] = 'Фото профиля';
+$lang['site_user_data_gender_label'] = 'Пол';
+$lang['site_user_data_gender_ph'] = 'Ваш пол';
+$lang['site_user_data_gender_null'] = 'Не указан'; //not specified
+$lang['site_user_data_gender_male'] = 'М';
+$lang['site_user_data_gender_female'] = 'Ж';
+$lang['site_user_data_'] = '';
