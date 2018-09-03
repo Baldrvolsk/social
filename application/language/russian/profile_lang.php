56a448ea74f04efc63ea06a2e1feedd6a22e1b89
Baldr <baldr.volsk@gmail.com>
2018-09-04 02:21:27 +0400

    add profile lang file

---
+++ application/language/russian/profile_lang.php
@@ -0,0 +1,11 @@
+<?php
+// rang
+$lang['profile_rang_name_delete'] = 'Удаленный пользователь';
+$lang['profile_rang_name_newbie'] = 'Новичок';
+$lang['profile_rang_name_member'] = 'Участник';
+$lang['profile_rang_name_verified'] = 'Верифицированный аккаунт';
+$lang['profile_rang_name_vip'] = 'V.I.P.';
+$lang['profile_rang_name_moderator'] = 'Модератор';
+$lang['profile_rang_name_admin'] = 'Администратор';
+$lang['profile_rang_name_sup_admin'] = 'Супер администратор';
+$lang['profile_rang_name_owner'] = 'Владелец';
