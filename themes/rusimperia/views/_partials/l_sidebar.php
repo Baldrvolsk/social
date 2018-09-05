9febdc5ae19429561509c944025d3984a2538307
Baldr <baldr.volsk@gmail.com>
2018-09-06 01:00:55 +0400

    верстка сайдбара

--- themes/rusimperia/views/_partials/l_sidebar.php
+++ themes/rusimperia/views/_partials/l_sidebar.php
@@ -1,14 +1,18 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
 
 <?php if ($this->ion_auth->logged_in()) : ?>
-    <div class="content-box l-sidebar">
+    <div class="content-box l-sidebar-menu">
         <ul class="page-list">
-            <li class="page-list-item">
+            <li class="page-list-item<?=(
+                ($this->uri->segment(1) === 'profile' &&
+                $this->uri->segment(2) === $this->user->id) ?
+                    ' active' : ''
+            )?>">
                 <a class="page-list-link" href="/profile">
                     Мой профиль
                 </a>
             </li>
-            <li class="page-list-item">
+            <li class="page-list-item<?=(($this->uri->segment(1) === 'friend')?' active':'')?>">
                 <a class="page-list-link" href="/friend">
                     Друзья
                     <span class="page-list-count" id="countFriend">
@@ -16,7 +20,7 @@
                     </span>
                 </a>
             </li>
-            <li class="page-list-item">
+            <li class="page-list-item<?=(($this->uri->segment(1) === 'chat')?' active':'')?>">
                 <a class="page-list-link" href="/chat">
                     Сообщения
                     <span class="page-list-count" id="countMessage">
@@ -24,10 +28,14 @@
                     </span>
                 </a>
             </li>
-            <li class="page-list-item">
-                <a class="page-list-link" href="/photo">Фотографии</a>
+            <li class="page-list-item<?=(
+                ($this->uri->segment(1) === 'photo' &&
+                $this->uri->segment(2) === 'user') ?
+                    ' active' : ''
+            )?>">
+                <a class="page-list-link" href="/photo/user">Фотографии</a>
             </li>
-            <li class="page-list-item">
+            <li class="page-list-item<?=(($this->uri->segment(1) === 'feed')?' active':'')?>">
                 <a class="page-list-link" href="/feed">
                     Новости
                     <span class="page-list-count" id="countNews">
@@ -35,7 +43,7 @@
                     </span>
                 </a>
             </li>
-            <li class="page-list-item">
+            <li class="page-list-item<?=(($this->uri->segment(1) === 'my_group')?' active':'')?>">
                 <a class="page-list-link" href="/my_group">
                     Мои группы
                     <span class="page-list-count" id="countGroup">
@@ -43,7 +51,7 @@
                     </span>
                 </a>
             </li>
-            <li class="page-list-item">
+            <li class="page-list-item<?=(($this->uri->segment(1) === 'rating')?' active':'')?>">
                 <a class="page-list-link" href="/rating">
                     &Delta;
                     <span class="page-list-count" id="countDelta">
@@ -51,7 +59,7 @@
                     </span>
                 </a>
             </li>
-            <li class="page-list-item">
+            <li class="page-list-item<?=(($this->uri->segment(1) === 'balance')?' active':'')?>">
                 <a class="page-list-link" href="/balance">
                     Лепта:
                     <span class="page-list-count" id="countLeptas">
@@ -62,34 +70,92 @@
         </ul>
         <ul class="group-list">
         <?php if (empty($groups)) : ?>
-            <li class="group-list-item">
+            <!--<li class="group-list-item">
                 <a class="group-list-link" href="/my_group">
                     Можете добавить до 5 групп в этот список
                 </a>
-            </li>
-        <?php else :
-        foreach ($groups as $row) : ?>
+            </li>-->
             <li class="group-list-item">
-                <a class="group-list-link" href="/gpoup/<?=$row->id?>">
-                    <?=$row->name?>
+                <a class="group-list-link" href="/group/1">
+                    <?=substr('Gcvbc sdsdf sdfsdfsd xcvdsx', 0, 20)?>
                 </a>
             </li>
-        <?php endforeach;
+            <li class="group-list-item">
+                <a class="group-list-link" href="/group/1">
+                    Группа #2
+                </a>
+            </li>
+            <li class="group-list-item">
+                <a class="group-list-link" href="/group/1">
+                    Группа #3
+                </a>
+            </li>
+            <li class="group-list-item">
+                <a class="group-list-link" href="/group/1">
+                    Группа #4
+                </a>
+            </li>
+            <li class="group-list-item">
+                <a class="group-list-link" href="/group/1">
+                    Группа #5
+                </a>
+            </li>
+        <?php else :
+            foreach ($groups as $row) : ?>
+                <li class="group-list-item<?=(
+                ($this->uri->segment(1) === 'group' &&
+                    $this->uri->segment(2) === $row->id) ?
+                    ' active' : ''
+                )?>">
+                    <a class="group-list-link" href="/group/<?=$row->id?>">
+                        <?=substr($row->name, 0, 20)?>
+                    </a>
+                </li>
+            <?php endforeach;
         endif; ?>
         </ul>
         <ul class="event-list">
             <?php if (empty($events)) : ?>
-                <li class="event-list-item">
+                <!--<li class="event-list-item">
                     <a class="event-list-link" href="/my_event">
                         Можете добавить до 3 мероприятий в этот список
+                    </a>
+                </li>-->
+                <li class="event-list-item">
+                    <a class="event-list-link" href="/event/1">
+                        <span class="event-title">Встреча #1</span>
+                        <span class="event-time">
+                            <?=date_to_str('%e %bs в %R', $this->router->user_lang, time())?>
+                        </span>
+                    </a>
+                </li>
+                <li class="event-list-item">
+                    <a class="event-list-link" href="/event/1">
+                        <span class="event-title">Встреча #2</span>
+                        <span class="event-time">
+                            <?=date_to_str('%e %bs в %R', $this->router->user_lang, time())?>
+                        </span>
+                    </a>
+                </li>
+                <li class="event-list-item">
+                    <a class="event-list-link" href="/event/1">
+                        <span class="event-title">Встреча #3</span>
+                        <span class="event-time">
+                            <?=date_to_str('%e %bs в %R', $this->router->user_lang, strtotime('2018-09-15 05:41:34'))?>
+                        </span>
                     </a>
                 </li>
             <?php else :
                 foreach ($events as $row) : ?>
-                    <li class="event-list-item">
+                    <li class="event-list-item<?=(
+                    ($this->uri->segment(1) === 'event' &&
+                        $this->uri->segment(2) === $row->id) ?
+                        ' active' : ''
+                    )?>">
                         <a class="event-list-link" href="/event/<?=$row->id?>">
-                            <span class="event-title"><?=$row->name?></span>
-                            <span class="event-time"><?=$row->time?></span>
+                            <span class="event-title"><?=substr($row->name, 0, 20)?></span>
+                            <span class="event-time"><?=date_to_str('%e %bs в %R', $this->router->user_lang,
+                                                                    strtotime($row->time))?></span>
                         </a>
                     </li>
                 <?php endforeach;
@@ -97,6 +163,6 @@
         </ul>
     </div>
 <?php endif; ?>
-<div class="advertising"><?=img_alt(180, 200, "advertising")?></div>
-<div class="advertising"><?=img_alt(180, 200, "advertising")?></div>
-<div class="advertising"><?=img_alt(180, 200, "advertising")?></div>
+<div class="advertising"><?=img_alt(190, 200, "advertising")?></div>
+<div class="advertising"><?=img_alt(190, 200, "advertising")?></div>
+<div class="advertising"><?=img_alt(190, 200, "advertising")?></div>
