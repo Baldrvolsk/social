be290945d6a6f5d7bf11ab05ef59a5c4366c52e6
Baldr <baldr.volsk@gmail.com>
2018-09-17 12:05:50 +0400

    заменено нахвание группы на id

--- application/language/russian/profile_lang.php
+++ application/language/russian/profile_lang.php
@@ -1,11 +1,11 @@
 <?php
 // rang
-$lang['profile_rang_name_delete'] = 'Удаленный пользователь';
-$lang['profile_rang_name_newbie'] = 'Новичок';
-$lang['profile_rang_name_member'] = 'Участник';
-$lang['profile_rang_name_verified'] = 'Верифицированный аккаунт';
-$lang['profile_rang_name_vip'] = 'V.I.P.';
-$lang['profile_rang_name_moderator'] = 'Модератор';
-$lang['profile_rang_name_admin'] = 'Администратор';
-$lang['profile_rang_name_sup_admin'] = 'Супер администратор';
-$lang['profile_rang_name_owner'] = 'Владелец';
+$lang['profile_rang_name_1'] = 'Удаленный пользователь';
+$lang['profile_rang_name_10'] = 'Новичок';
+$lang['profile_rang_name_20'] = 'Участник';
+$lang['profile_rang_name_30'] = 'Верифицированный аккаунт';
+$lang['profile_rang_name_40'] = 'V.I.P.';
+$lang['profile_rang_name_100'] = 'Модератор';
+$lang['profile_rang_name_200'] = 'Администратор';
+$lang['profile_rang_name_201'] = 'Супер администратор';
+$lang['profile_rang_name_202'] = 'Владелец';
