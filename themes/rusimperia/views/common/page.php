d882d3e8f01ab27537e887879d19530b54d1d524
Baldr <baldr.volsk@gmail.com>
2018-09-17 12:09:32 +0400

    верстка социальных кнопок

--- themes/rusimperia/views/common/page.php
+++ themes/rusimperia/views/common/page.php
@@ -3,11 +3,13 @@
     <h1 class="page-title"><?=$h?></h1>
     <div class="page-content"><?=$content?></div>
 </div>
-<div class="social">
-    <span><?=$this->lang->line('site_social_text')?></span>
-    <?php foreach ($this->config->item('site_social') as $row) : ?>
-    <a href="<?=$row['link']?>" title="<?=$this->lang->line('site_social_title_'.$row['name'])?>" target="_blank">
-        <i class="fab <?=$row['faIcon']?> fa-3x"></i>
-    </a>
-    <?php endforeach; ?>
+<div class="social-wrap">
+    <div class="content-box social">
+        <span><?=$this->lang->line('site_social_text')?></span>
+        <?php foreach ($this->config->item('site_social') as $row) : ?>
+        <a href="<?=$row['link']?>" title="<?=$this->lang->line('site_social_title_'.$row['name'])?>" target="_blank">
+            <i class="fab <?=$row['faIcon']?> fa-2x"></i>
+        </a>
+        <?php endforeach; ?>
+    </div>
 </div>
