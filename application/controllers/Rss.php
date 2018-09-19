1884b089bdd3d473c361b09f98ddcd06232e4297
Andrey Chepurnov <achepurnov86@gmail.com>
2018-09-19 21:19:33 +1000

    добавлен файл RSS контроллера

---
+++ application/controllers/Rss.php
@@ -0,0 +1,27 @@
+<?php
+
+
+class Rss extends CI_Controller
+{
+    public $user;
+
+    public function __construct() {
+        parent::__construct();
+        if (!$this->ion_auth->logged_in()) {
+            redirect('', 'refresh');
+        } else {
+            $this->user = $this->ion_auth->user()->row();
+        }
+    }
+
+    public function index() {
+        $debug = array();
+        $this->theme
+            ->title('Лента новостей')
+            ->add_partial('header')
+            ->add_partial('l_sidebar')
+            ->add_partial('r_sidebar')
+            ->add_partial('footer', $debug)
+            ->load('common/in_dev');
+    }
+}
