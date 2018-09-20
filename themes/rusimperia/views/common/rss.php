5463db4e198833f543b45f9c9fc3badff882e26b
Andrey Chepurnov <achepurnov86@gmail.com>
2018-09-20 22:01:53 +1000

    Добавил статистически закодированный список каналов в RSS view

--- themes/rusimperia/views/common/rss.php
+++ themes/rusimperia/views/common/rss.php
@@ -1,5 +1,12 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
 <h2>RSS лента</h2>
+<ul>
+	<li><a href='http://www.rusimperia.info/rss/news/all.xml'>http://www.rusimperia.info/rss/news/all.xml</a></li>
+	<li><a href='http://inosmi.ru/export/rss2/index.xml'>http://inosmi.ru/export/rss2/index.xml</a></li>
+	<li><a href='http://www.vedomosti.ru/info/rss'>http://www.vedomosti.ru/info/rss</a></li>
+	<li><a href='https://rbc.ru'>https://rbc.ru</a></li>
+	<li><a href='http://www.pravoslavie.ru/rss/ '>http://www.pravoslavie.ru/rss/</a></li>
+</ul>
 <?php
 	echo print_r($RSS);
 ?>	
