4b7a7ac32867473f05ed4137de17272954040871
Baldr <baldr.volsk@gmail.com>
2018-09-24 15:44:04 +0400

    1. change debug 2. add modal 3. add js var controller

--- themes/rusimperia/views/_partials/footer.php
+++ themes/rusimperia/views/_partials/footer.php
@@ -1,18 +1,32 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
 <?php if (DEBUG) : ?>
-    <div class="debug">
-        <details>
-            <summary>Отладочная информация</summary>
-            <div class="debug-info">
-                <?php if (!empty($debug)) :
-                foreach ($debug as $var) :?>
-                <details>
-                    <summary><?=$var['t']?></summary>
-                    <pre><?=$var['c']?></pre>
-                </details>
-                <?php endforeach;
-                endif; ?>
-            </div>
-        </details>
-    </div>
+<!-- Debug Info -->
+<div class="debug">
+    <details>
+        <summary>Отладочная информация</summary>
+        <span class='btn test-open-modal'>Test Open Modal</span>
+        <div class="debug-info">
+            <?php if (!empty($debug)) :
+            foreach ($debug as $var) :?>
+            <details>
+                <summary><?=$var['t']?></summary>
+                <pre><?=$var['c']?></pre>
+            </details>
+            <?php endforeach;
+            endif; ?>
+        </div>
+    </details>
+</div>
 <?php endif; ?>
+<!-- Modal -->
+<div class="overlay">
+    <div class="content-box modal" id="modal">
+        <span class="btn-close-modal fas fa-times fa-2x"></span>
+        <div class="modal-header"></div>
+        <div class="modal-body"></div>
+        <div class="modal-footer"></div>
+    </div>
+</div>
+<script>
+    var controller = '<?=strtolower($this->router->class)?>';
+</script>
