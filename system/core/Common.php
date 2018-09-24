3fb7b22cb1e4acfb18276e3177dd42b28ed03312
Baldr <baldr.volsk@gmail.com>
2018-09-24 15:41:04 +0400

    change debug view function

--- system/core/Common.php
+++ system/core/Common.php
@@ -776,12 +776,60 @@ if (!function_exists('function_usable')) {
     }
 }
 
-if (!function_exists('var_debug')) {
-    function var_debug($var) {
+if (!function_exists('pretty_print')) {
+    /**
+     * Выводит массив в виде дерева
+     *
+     * @param mixed - Массив или объект, который надо обойти
+     * @param boolean - Раскрыть дерево элементов по-умолчанию или нет?
+     *
+     * @return string
+     */
+    function pretty_print($in, $opened = false) {
         ob_start();
-        var_dump($var);
+        if ($opened) {
+            $opened = ' open';
+        }
+        if (is_object($in) or is_array($in)) {
+            echo '<div>';
+            echo '<details' . $opened . '>';
+            echo '<summary>';
+            echo (is_object($in)) ? 'Object {' . count((array)$in) . '}' : 'Array [' . count($in) . ']';
+            echo '</summary>';
+            pretty_print_rec($in, $opened);
+            echo '</details>';
+            echo '</div>';
+        }
         $c = ob_get_contents();
         ob_end_clean();
         return $c;
+    }
+
+    function pretty_print_rec($in, $opened, $margin = 10) {
+        if (!is_object($in) && !is_array($in)) {
+            return;
+        }
+
+        foreach ($in as $key => $value) {
+            if (is_object($value) or is_array($value)) {
+                echo '<details style="margin-left:' . $margin . 'px" ' . $opened . '>';
+                echo '<summary>';
+                echo (is_object($value)) ? $key . ' {' . count((array)$value) . '}' : $key . ' [' . count($value) . ']';
+                echo '</summary>';
+                pretty_print_rec($value, $opened, $margin + 10);
+                echo '</details>';
+            } else {
+                switch (gettype($value)) {
+                    case 'string':
+                        $bgc = 'red';
+                        break;
+                    case 'integer':
+                        $bgc = 'green';
+                        break;
+                }
+                echo '<div style="margin-left:' . $margin . 'px">' . $key . ' : <span style="color:' . $bgc . '">' .
+                    $value . '</span> (' . gettype($value) . ')</div>';
+            }
+        }
     }
 }
