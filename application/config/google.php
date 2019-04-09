78356c540602946986bf19f037e904997ab123f0
Ivan Semin <baldr.volsk@gmail.com>
2019-04-09 12:04:00 +0400

    delete google api-key

--- application/config/google.php
+++ application/config/google.php
@@ -10,9 +10,9 @@
 |  api_key           string   Developer key.
 |  scopes            string   Specify scopes
 */
-$config['google']['client_id']        = '430678797387-kd89me01nm4g68ntl1kbtdf8e0e3he6m.apps.googleusercontent.com';
-$config['google']['client_secret']    = 'p9VbQvqLyJs6rpEF0vUB_ViS';
-$config['google']['redirect_uri']     = 'https://rusimperia.ru/auth/login';
-$config['google']['application_name'] = 'Login to Rusimperia.ru';
-$config['google']['api_key']          = 'AIzaSyAzsRx91oMKccOWaYLmlHNcigjAQUFh7rc';
+$config['google']['client_id']        = '********';
+$config['google']['client_secret']    = '********';
+$config['google']['redirect_uri']     = '********';
+$config['google']['application_name'] = 'Login to social.org';
+$config['google']['api_key']          = '********';
 $config['google']['scopes']           = array();
